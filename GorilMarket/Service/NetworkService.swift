eee07368bd7a9f226187cd32891edd753b692ecd
Utku Çetinkaya <utku.cetinkaya@hotmail.com>
2023-10-14 20:47:16 +0300

    Network layer was written with Alamofire using URLRequestConvertible.

--- GorilMarket/Service/NetworkService.swift
+++ GorilMarket/Service/NetworkService.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import Alamofire
+
+protocol NetworkService {
+    func request<T: Decodable>(_ router: Router,Type: T.Type, completion: @escaping (Result<T, Error>) -> Void)
+}
+
+class AlamofireNetworkService: NetworkService {
+
+    static let shared = AlamofireNetworkService()
+    private init() {}
+
+    func request<T: Decodable>(_ router: Router, Type type: T.Type, completion: @escaping (Result<T, Error>) -> Void) {
+
+        var request = router.request()
+
+        // Add parameters to the request body
+        if let parameters = router.parameters {
+            do {
+                let data = try JSONSerialization.data(withJSONObject: parameters)
+                request.httpBody = data
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+
+        // Make the network request
+        AF.request(request).responseDecodable(of: T.self, completionHandler: { response in
+            switch response.result {
+            case .success(let decodedData):
+                completion(.success(decodedData))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        })
+    }
+}
--- GorilMarket/Service/Router.swift
+++ GorilMarket/Service/Router.swift
@@ -6,3 +6,87 @@
 //
 
 import Foundation
+import Alamofire
+
+ protocol URLRequestConvertible {
+     var url: URL { get }
+     var method: HTTPMethod { get }
+     var parameters: [String: Any]? { get }
+     var headers: [String: String]? { get }
+     func request() -> URLRequest
+ }
+
+ enum Router {
+     
+     case tumYemekleriGetir
+     case sepettekiYemekleriGetir
+     case sepeteYemekEkle(yemekId: Int)
+     case sepettenYemekSil(yemekId: Int)
+     case yemekResmiGetir(resimAdi: String)
+ }
+
+ extension Router: URLRequestConvertible {
+    
+     static let baseURL = "https://kasimadalan.pe.hu/yemekler"
+     
+     var url: URL {
+         switch self {
+         case .tumYemekleriGetir:
+             return URL(string: Router.baseURL + "/tumYemekleriGetir.php")!
+         case .sepettekiYemekleriGetir:
+             return URL(string: Router.baseURL + "/sepettekiYemekleriGetir.php")!
+         case .sepeteYemekEkle(let yemekId):
+             return URL(string: Router.baseURL + "/sepeteYemekEkle.php?yemekId=\(yemekId)")!
+         case .sepettenYemekSil(let yemekId):
+             return URL(string: Router.baseURL + "/sepettenYemekSil.php?yemekId=\(yemekId)")!
+         case .yemekResmiGetir(let resimAdi):
+             return URL(string: Router.baseURL + "/resimler/\(resimAdi)")!
+         }
+     }
+     
+     var method: HTTPMethod {
+         switch self {
+         case .tumYemekleriGetir, .sepettekiYemekleriGetir, .yemekResmiGetir:
+             return .get
+         case .sepeteYemekEkle, .sepettenYemekSil:
+             return .post
+         }
+     }
+     
+     var parameters: [String : Any]? {
+         switch self {
+         case .tumYemekleriGetir, .sepettekiYemekleriGetir, .yemekResmiGetir:
+             return [:]
+         case .sepeteYemekEkle(let yemekId):
+             return ["yemekId": yemekId]
+         case .sepettenYemekSil(let yemekId):
+             return ["yemekId": yemekId]
+         }
+     }
+     
+     var headers: [String : String]? {
+         return [:]
+     }
+     
+     func request() -> URLRequest {
+         // Create request
+         var request = URLRequest(url: url)
+         request.httpMethod = method.rawValue
+         // Add Parameters
+         if let parameters = parameters {
+             do {
+                 let data = try JSONSerialization.data(withJSONObject: parameters)
+                 request.httpBody = data
+             } catch {
+                 print(error.localizedDescription)
+             }
+         }
+         // Add Headers
+         if let headers = headers {
+             for (key, value) in headers {
+                 request.setValue(value, forHTTPHeaderField: key)
+             }
+         }
+         return request
+     }
+ }
